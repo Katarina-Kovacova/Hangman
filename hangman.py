6f21f67b3234ea802b3d5e9b21617534910f5ac6
kitkatnotchunky <125378069+kitkatnotchunky@users.noreply.github.com>
2023-02-27 21:26:49 +0000

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,48 @@
+#Hangman
+# Import random word library
+from random_word import RandomWords
+r = RandomWords()
+# Return a single random word
+ran_word = r.get_random_word()
+print(ran_word)
+#hide the word
+hidden_ran_word = '*' * len(ran_word)
+print(hidden_ran_word)
+
+
+
+
+#ask player to guess a letter
+
+while True:
+	guess = str(input('Guess a letter: '))
+	global guess
+	if guess.isalpha(): #possibly call the is_in_ran_word function?
+		print('Thank you')
+		break
+	else:
+		print('Only enter letters please! ')
+#Convert random word to list
+hidden_ran_word = list(hidden_ran_word)
+
+#check if letter in secret word
+def is_in_ran_word():
+	
+
+
+        
+
+
+
+
+
+
+
+
+ 	
+ 	
+# else:
+	# print('No such letter in this word')
+	# print( part of hangman)
+
+
