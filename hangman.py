4b0ebe0df3d2767e3b2c1cb3af77ba669b1e405b
kitkatnotchunky <125378069+kitkatnotchunky@users.noreply.github.com>
2023-03-13 21:49:16 +0000

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,18 +3,18 @@
 from random_word import RandomWords
 
 def update_hidden_word(positions, ran_word): # this function will disclose correct positions in hidden_word and hide not guessed letter
-    #print('*****----',guess) #checking if any guess is passed into this function
-    result = []
-    # This is going to be positions will equal 0 and 2. Ran word is CAT
-    for i, char in enumerate(ran_word): 
-        if i not in positions: 
-        	# 1 is not in positions therefore we can expose index 1 (which is A)
-        	result.append(char)        	            
-        else:
-        	# 0 and 2 are not in positions therefore we must return * to keep them hidden
-        	result.append('*')        	
-    return ''.join(result) # '**e'
-        	# print(guess, i, '. position')
+	#print('*****----',guess) #checking if any guess is passed into this function
+	result = []
+	# This is going to be positions will equal 0 and 2. Ran word is CAT
+	for i, char in enumerate(ran_word): 
+		if i not in positions: 
+			# 1 is not in positions therefore we can expose index 1 (which is A)
+			result.append(char)        	            
+		else:
+			# 0 and 2 are not in positions therefore we must return * to keep them hidden
+			result.append('*')        	
+	return ''.join(result) # '**e'
+			# print(guess, i, '. position')
   #NOW NEED TO CHECK IF THE GUESSED RESULT MATCHES HIDDEN POSITIONS AND REMOVE THOSE IN HIDDEN POSITIONS
 
 def get_correct_positions(ran_word, guess):	#getting correct positions in the random word which we then use to update what stays hidden and what gets disclosed
@@ -23,7 +23,8 @@ def get_correct_positions(ran_word, guess):	#getting correct positions in the ra
 		if guess == char:
 			result.append(i)
 	return result
-
+		
+   
 def get_letter(): # get a letter from user
 	guess = ''
 	while not (len(guess) == 1 and guess.isalpha()):
@@ -33,12 +34,48 @@ def get_letter(): # get a letter from user
 
 	# create guessed_letters, if guess not in guessed_letter - append to guessed_letter, else print already guessed
 
-def update_hidden_positions(positions, correct_positions):
-    for position in correct_positions:
-    	positions.remove(position)
-    return positions
+def update_hidden_positions(keep_hidden, remove_guessed_positions):
+	# print(remove_guessed_positions)
+	# print(keep_hidden)
+
+	for position in remove_guessed_positions:
+		keep_hidden.remove(position)
+		print(position)
+
+	return keep_hidden
+
+def update_guessed_leters (guess, guessed_letters): #function will update the list with already guessed letters and return updated list
+	guessed_letters.append(guess)
+	guessed_letters_set = set(guessed_letters)
+	print(guessed_letters_set)
+	print(list(guessed_letters_set))
+	return list(guessed_letters_set)
+
+def get_hidden_string(positions, ran_word):
+	result = []
+	# cat 0, 2 
+	# *a*
+	for i, char in enumerate(ran_word):
+		if i in positions: # positions are what has yet not been guessed correctly
+			result.append('*')
+		else:
+			result.append(char)
+	return ''.join(result)
+
+	return result
+
+def get_body_parts(number_of_wrong_guesses):
+	body_parts = ['head', 'neck', 'torso', 'right arm', 'left arm', 'right leg', 'left leg']
+	# return body_parts[0:3] 
+	return '\n'.join(body_parts[0:number_of_wrong_guesses])
+
+	
+
+
+
 
 def main():
+	
 	
 	r = RandomWords()
 	# Return a single random word
@@ -54,22 +91,43 @@ def main():
 	hidden_positions = list(range(len(hidden_ran_word)))
 	print(hidden_positions)
 
+	guessed_letters = []
+	bad_guesses = 0
 	game_over = False
 	while not game_over:
 		guess = get_letter() # check if letter is in word
-		if guess in ran_word:
-			 
-			 hidden_positions = update_hidden_positions(hidden_positions, get_correct_positions(ran_word, guess))
-			 if hidden_positions == []:
-			 	print('Congratulations')
-			 	game_over = True
-			 print(update_hidden_word(hidden_positions, ran_word))
+		
+
+		if guess in guessed_letters:
+			print('You already guessed that letter')
 		else:
-			print('Guess again, this is the info', hidden_ran_word)
-    
- 
+			guessed_letters = update_guessed_leters(guess, guessed_letters)
+
+			if guess in ran_word:
+				 
+				hidden_positions = update_hidden_positions(hidden_positions, get_correct_positions(ran_word, guess))
+				if hidden_positions == []:
+					print('Congratulations, you have guessed the letter.')
+					game_over = True
+				print(update_hidden_word(hidden_positions, ran_word))
+			else:
+				# bad_guesses = bad_guesses + 1 (same as line 115)
+				bad_guesses += 1
+				print(get_body_parts(bad_guesses))
+				print('Guess again, this is the info', get_hidden_string(hidden_positions, ran_word))
+	   
+		
 if __name__ == '__main__':
-    main()
+	main()
+	
+quit the game when they got all body parts printed 
+stop printnig letter position when guessed correctly
+
+
+
+
+
+
     
 
 
